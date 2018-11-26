84ba2bb60dfc30aed15a72f030d48daf08b9bfe2
Vito Liang <xxliangvictorxx@gmail.com>
2018-11-25 22:15:47 -0800

    search done, case sensitive

--- PostMishone/DescribeMissionViewController.swift
+++ PostMishone/DescribeMissionViewController.swift
@@ -10,14 +10,47 @@ import Firebase
 import MapKit
 import CoreLocation
 
-class DescribeMissionViewController: UIViewController {
+class DescribeMissionViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    
     var ref: DatabaseReference!
     var latitude = 0.0
     var longitude = 0.0
+    var selectedPriority : String?
+    var missionCategories = ["Deliver", "Partner", "Rent", "Other"]
+    var myPickerView : UIPickerView!
+
     
     @IBOutlet weak var missionName: UITextField!
     @IBOutlet weak var missionDescription: UITextField!
     @IBOutlet weak var reward: UITextField!
+    @IBOutlet weak var missionCategory: UITextField!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return missionCategories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return missionCategories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.missionCategory.text = missionCategories[row]
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickUp(missionCategory)
+    }
+    
+    func createPickerView() {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        
+        missionCategory.inputView = pickerView
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -30,6 +63,8 @@ class DescribeMissionViewController: UIViewController {
         missionName.delegate = self
         missionDescription.delegate = self
         reward.delegate = self
+
+        pickUp(missionCategory) // Load picker view (to select categories)
         
         // Do any additional setup after loading the view.
         ref = Database.database().reference() // Firebase Reference
@@ -48,11 +83,12 @@ class DescribeMissionViewController: UIViewController {
         print("missionNameText: ", missionName.text!)
         print("missionDescription: ", missionDescription.text!)
         print("reward: ", reward.text!)
+        print("category: ", missionCategory.text!)
    
         
         let missionID = ref.child("PostedMissions").childByAutoId().key
         // Add to https://postmishone.firebaseio.com/PostedMissions
-        ref?.child("PostedMissions").child(missionID!).setValue(["Latitude": latitude, "Longitude": longitude, "UserID": userID, "timeStamp": timeStamp, "missionName": missionName.text!, "missionDescription": missionDescription.text!, "reward": reward.text!, "missionID": missionID!])
+        ref?.child("PostedMissions").child(missionID!).setValue(["Latitude": latitude, "Longitude": longitude, "UserID": userID, "timeStamp": timeStamp, "missionName": missionName.text!, "missionDescription": missionDescription.text!, "reward": reward.text!, "missionID": missionID!, "category" : missionCategory.text!])
         
         // Add to https://postmishone.firebaseio.com/users/(currentuserid)/
         ref?.child("Users").child(userID).child("MissionPosts").child(missionID!).setValue(missionID!)
@@ -64,15 +100,58 @@ class DescribeMissionViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-}
-
-// Make keyboard close on return key press
-extension DescribeMissionViewController : UITextFieldDelegate {
     
+    // Make keyboard close on return key press
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         textField.resignFirstResponder()
         return true
     }
+    
+    func dismissPickerView() {
+        let toolBar = UIToolbar()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: "donePicker")
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: "donePicker")
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        missionCategory.inputAccessoryView = toolBar
+
+    }
+    
+
+    func pickUp(_ textField : UITextField){
+        // UIPickerView
+        self.myPickerView = UIPickerView(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.myPickerView.delegate = self
+        self.myPickerView.dataSource = self
+        self.myPickerView.backgroundColor = UIColor.white
+        textField.inputView = self.myPickerView
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(DescribeMissionViewController.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(DescribeMissionViewController.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+        
+    }
+    
+    @objc func doneClick() {
+        missionCategory.resignFirstResponder()
+    }
+    @objc func cancelClick() {
+        missionCategory.resignFirstResponder()
+    }
+    
     
 }
--- PostMishone/SearchMission.swift
+++ PostMishone/SearchMission.swift
@@ -13,7 +13,9 @@ import Firebase
 class SearchMission: UIViewController {
     var ref: DatabaseReference!
     var searchedMissions = [Mission]()
-    var isSearching = false
+    var didSelectCategory = false
+    var selectedCategory = ""
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -63,8 +65,18 @@ extension SearchMission: UISearchBarDelegate {
                 for child in snapshot.children {
                     print("FOR LOOP")
                     let snap = child as! DataSnapshot
-                    if let dic = snap.value as? [String:Any], let missionName = dic["missionName"] as? String, let missionDescription = dic["missionDescription"] as? String, let reward = dic["reward"] as? String {
-                        self.searchedMissions.append(Mission(missionName: missionName, missionDescription: missionDescription, missionReward: reward))
+                    if let dic = snap.value as? [String:Any], let missionName = dic["missionName"] as? String, let missionDescription = dic["missionDescription"] as? String, let reward = dic["reward"] as? String, let category = dic["category"] as? String {
+                        if(self.didSelectCategory) {
+                            print("didSelectCategory")
+                            if(self.selectedCategory == category) {
+                                self.searchedMissions.append(Mission(missionName: missionName, missionDescription: missionDescription, missionReward: reward, missionCategory: category))
+                            }
+                        }
+                        else {
+                            self.searchedMissions.append(Mission(missionName: missionName, missionDescription: missionDescription, missionReward: reward, missionCategory: category))
+                        }
+
+
                         print(self.searchedMissions.count)
                         self.tableView.reloadData()
                     }
@@ -73,6 +85,56 @@ extension SearchMission: UISearchBarDelegate {
         }
 
     }
+    
+        func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+            switch selectedScope {
+            case 0:
+                print("all")
+                searchBar.endEditing(true)
+                searchedMissions.removeAll()
+                searchBar.text = ""
+                tableView.reloadData()
+                didSelectCategory = false
+                break
+            case 1:
+                print("deliver")
+                searchBar.endEditing(true)
+                searchedMissions.removeAll()
+                searchBar.text = ""
+                tableView.reloadData()
+                didSelectCategory = true
+                selectedCategory = "Deliver"
+
+            case 2:
+                print("partner")
+                searchBar.endEditing(true)
+                searchedMissions.removeAll()
+                searchBar.text = ""
+                tableView.reloadData()
+                didSelectCategory = true
+                selectedCategory = "Partner"
+
+            case 3:
+                print("rent")
+                searchBar.endEditing(true)
+                searchedMissions.removeAll()
+                searchBar.text = ""
+                tableView.reloadData()
+                didSelectCategory = true
+                selectedCategory = "Rent"
+            case 4:
+                print("other")
+                searchBar.endEditing(true)
+                searchedMissions.removeAll()
+                searchBar.text = ""
+                tableView.reloadData()
+                didSelectCategory = true
+                selectedCategory = "Other"
+            default:
+                didSelectCategory = false
+                break
+            }
+        }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.endEditing(true)
@@ -89,10 +151,12 @@ class Mission {
     let missionName: String
     let missionDescription: String
     let missionReward: String
+    let missionCategory: String
     
-    init(missionName: String, missionDescription: String, missionReward: String) {
+    init(missionName: String, missionDescription: String, missionReward: String, missionCategory: String) {
         self.missionName = missionName
         self.missionDescription = missionDescription
         self.missionReward = missionReward
+        self.missionCategory = missionCategory
     }
 }
